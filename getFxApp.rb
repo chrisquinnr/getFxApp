7afe002bb971d1fbbe842573ffd8de81a04b620c
Chris Quinn <chris@gravitywell.co.uk>
2017-03-15 21:05:36 +0000

    added actual values, outputs, css flags matching currency codes and some more layout goodness

--- getFxApp.rb
+++ getFxApp.rb
@@ -4,11 +4,12 @@ require 'getFx'
 
 get'/' do
   output = GetFx.run([])
-  erb :index, :locals => {:output => output}
+  erb :index, :locals => {:output => output, :fromflag => 'usd', :from => 'USD', :toflag => 'eur', :to => 'EUR', :date => '2017-02-02', :amt => 1 }
 end
 
 get '/?:curr/?:date/?:amt' do
   p = params["captures"]
   output = GetFx.run(p)
-  erb :index, :locals => {:output => output}
+  erb :index, :locals => {:output => output, :fromflag => p[0].downcase, :from => p[0], :toflag => 'eur', :to => 'EUR', :date => p[1], :amt => p[2] }
 end
+
